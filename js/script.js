2aeb1a25086983a978b9ac40b28479392590aa3e
xCcastrox <110793856+xCcastrox@users.noreply.github.com>
2022-11-02 15:22:00 -0400

    single line comm, mutli comm,name, foods

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Single line comment //
+/* Multi-line comment */
+const myObj = {firstname:"Christopher", lastname:"Castro"};
+console.log(myObj);
+
+const myArr =["Mango, Ramen, Tacos, BBQ, Italian, Cake"];
+console.log(myArr);
